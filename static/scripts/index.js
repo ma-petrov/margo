c68979d721c156aa9ddd1660d90025b026668b15
Petrov Mikhail <petrov.ma@icloud.com>
2022-08-03 17:58:35 +0300

    v1

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,12 +1,1 @@
 const aboutMe = document.getElementById("about-me");
-const cv = document.getElementById("cv");
-
-document.getElementById("open-cv-button").addEventListener("click", () => {
-    aboutMe.style.top = "-100vh";
-    cv.style.top = "0vh";
-});
-
-document.getElementById("close-cv-button").addEventListener("click", () => {
-    aboutMe.style.top = "0vh";
-    cv.style.top = "100vh";
-});
